f0e6db2103535bc394bfc84cb6dd6eccd87446c1
Jay Patravali <jaypatravali@gmail.com>
2020-04-24 01:47:43 -0700

    follow left, right and error

---
+++ pid_ws/src/algorithms/wall_following/scripts/pid_error.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+
+import rospy
+import math
+import numpy as np
+import yaml
+import sys
+from sensor_msgs.msg import LaserScan
+from std_msgs.msg import Float64
+import pdb
+
+pub = rospy.Publisher('pid_error', Float64, queue_size=10)
+
+# You can define constants in Python as uppercase global names like these.
+MIN_DISTANCE = 0.1
+MAX_DISTANCE = 30.0
+MIN_ANGLE = -45.0
+MAX_ANGLE = 225.0
+LOOKAHEAD_DISTANCE = 2.5
+DESIRED_DISTANCE = 0.2
+THETA = 45
+# data: single message from topic /scan
+# angle: between -45 to 225 degrees, where 0 degrees is directly to the right
+# Outputs length in meters to object with angle in lidar scan field of view
+def getRange(data, angle):
+
+
+  data_arr = np.array(data.ranges)
+  #filter data
+  data_arr[data_arr>MAX_DISTANCE] = MAX_DISTANCE
+  data_arr[data_arr<MIN_DISTANCE] = MIN_DISTANCE
+
+  # check if within fixed angle bounds 
+  if angle<MIN_ANGLE:
+    angle = MIN_ANGLE
+  elif angle>MAX_ANGLE:
+    angle = MAX_ANGLE
+
+  # find Index of range value
+  ranges_idx = int((angle - MIN_ANGLE)/np.degrees(data.angle_increment))
+  range_val = data_arr[ranges_idx]
+
+  return range_val
+
+
+def compute_projectedDistance(a, b):
+    num = a*np.cos(np.radians(THETA)) - b
+    denom= a*np.sin(np.radians(THETA))
+    alpha = np.arctan(num/denom)
+
+    Dt = b*np.cos(alpha)
+    Dt_projected = Dt + LOOKAHEAD_DISTANCE*np.sin(alpha)
+    return Dt_projected
+
+# data: single message from topic /scan
+# desired_distance: desired distance to the left wall [meters]
+# Outputs the PID error required to make the car follow the left wall.
+def followLeft(data, desired_distance):
+
+  #diametrically opposite to right side.
+  a = getRange(data, 180)
+  b = getRange(data, 180 - THETA)
+
+  Dt_projected =compute_projectedDistance(a,b)
+  left_error =  desired_distance- Dt_projected
+
+  rospy.loginfo('left error %f', left_error)
+
+
+  return left_error
+
+# data: single message from topic /scan
+# desired_distance: desired distance to the right wall [meters]
+# Outputs the PID error required to make the car follow the right wall.
+def followRight(data, desired_distance):
+
+  a = getRange(data, 0)
+  b = getRange(data, THETA)
+
+  Dt_projected = compute_projectedDistance(a,b)
+  print (Dt_projected, desired_distance)
+  right_error = Dt_projected - desired_distance
+
+  rospy.loginfo('right error %f', right_error)
+
+  return right_error
+
+# data: single message from topic /scan
+# Outputs the PID error required to make the car drive in the middle
+# of the hallway.
+def followCenter(data):
+  # TODO: implement
+
+
+
+  return 0.0
+
+# Callback for receiving LIDAR data on the /scan topic.
+# data: the LIDAR data, published as a list of distances to the wall.
+def scan_callback(data):
+  # error = followRight(data, DESIRED_DISTANCE)
+  error = followLeft(data, DESIRED_DISTANCE)
+
+
+  msg = Float64()
+  msg.data = error
+  pub.publish(msg)
+
+# Boilerplate code to start this ROS node.
+# DO NOT MODIFY!
+if __name__ == '__main__':
+  rospy.init_node('pid_error_node', anonymous = True)
+  rospy.Subscriber("scan", LaserScan, scan_callback)
+  rospy.spin()
---
+++ pid_ws/src/patravali_wall_following/scripts/control.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+
+import rospy
+from race.msg import drive_param
+from std_msgs.msg import Float64
+import numpy as np
+import math
+
+KP = -0.12
+KD = 0.00
+ANGLE_LEVEL_1 = 10.0
+SPEED_LEVEL_1 = 1.5
+ANGLE_LEVEL_2 = 20.0
+SPEED_LEVEL_2 = 1.0
+SPEED_LEVEL_3 = 0.5
+
+past_error = 0.0
+past_timeStamp = 0.0
+flag = False
+pub = rospy.Publisher('drive_parameters', drive_param, queue_size=1)
+
+# Callback for receiving PID error data on the /pid_error topic
+# data: the PID error from pid_error_node, published as a Float64
+def control_callback(msg):
+
+    curr_error = msg.data
+    curr_timeStamp = rospy.Time.now().to_sec()
+
+    global past_error
+    global past_timeStamp
+    global flag
+    delta_time = abs(curr_timeStamp - past_timeStamp)
+    delta_error = curr_error - past_error
+
+    past_error = curr_error
+    past_timeStamp = curr_timeStamp
+
+    if flag:
+        pidoutput = KP*curr_error + KD*(delta_error/delta_time)
+    else:
+        flag = True
+        pidoutput = KP*curr_error
+
+
+    angle = math.radians(pidoutput)    #convert the angle to radians if not already in radians
+    angle = np.clip(angle, -0.4189, 0.4189)  # 0.4189 radians = 24 degrees because car can only turn 24 degrees max
+
+    if abs(angle) < ANGLE_LEVEL_1:
+        vel =  SPEED_LEVEL_1
+    elif  ANGLE_LEVEL_1<= abs(angle) and abs(angle) < ANGLE_LEVEL_2:
+    # elif abs(angle) < ANGLE_LEVEL_2:        
+        vel =  SPEED_LEVEL_2
+    else:
+        vel =  SPEED_LEVEL_3
+    # print (vel, angle)
+    msg = drive_param()
+    msg.velocity = vel  # TODO: implement PID for velocity
+    msg.angle = angle    # TODO: implement PID for steering angle
+    pub.publish(msg)
+
+# Boilerplate code to start this ROS node.
+if __name__ == '__main__':
+    rospy.init_node('pid_controller_node', anonymous=True)
+    rospy.Subscriber("pid_error", Float64, control_callback)
+    rospy.spin()
+
